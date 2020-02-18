720f429bf75dac546dff3198ff731284b0b71bfb
jfcarocota <jfcarocota@gmail.com>
2020-02-17 22:12:53 -0700

    Updates

---
+++ precache-manifest.406ff500ad560b488530ab8af1a1b2eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad329245e0901740e2448ae8fd4098ad",
+    "url": "/amv-psicologia/index.html"
+  },
+  {
+    "revision": "9482c91ca294cd308985",
+    "url": "/amv-psicologia/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4717dd7a4ff02af2b6b5",
+    "url": "/amv-psicologia/static/js/2.3dabd2a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/amv-psicologia/static/js/2.3dabd2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9482c91ca294cd308985",
+    "url": "/amv-psicologia/static/js/main.48659ab4.chunk.js"
+  },
+  {
+    "revision": "76133daa3ba5cc254348",
+    "url": "/amv-psicologia/static/js/runtime-main.f95324e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amv-psicologia/static/media/logo.5d5d9eef.svg"
+  }
+]);
